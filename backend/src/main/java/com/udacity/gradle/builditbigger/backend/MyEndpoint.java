4bcea94f7778766a95c8323189b297b76c1cb4e1
Sid Price <sid@sidprice.com>
2018-08-12 17:24:54 -0600

    GCE using the JokeProvider

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.sidprice.android.jokesprovider.Jokes;
 
 import javax.inject.Named;
 
@@ -33,7 +34,9 @@ public class MyEndpoint {
     @ApiMethod(name = "getJoke")
     public MyBean getJoke() {
         MyBean  response = new MyBean() ;
-        response.setData("Silly joke");
+        Jokes   jokes = new Jokes() ;
+
+        response.setData(jokes.getJoke());
         return response ;
     }
 }
