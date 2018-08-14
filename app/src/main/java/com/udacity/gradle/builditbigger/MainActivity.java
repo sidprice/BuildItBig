d659d7e4d146ee183c87cba6a835322b2d6e3464
Sid Price <sid@sidprice.com>
2018-08-14 10:21:45 -0600

    Added Toast when joke not received

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -46,11 +46,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         new EndpointsAsyncTask().execute(this) ;
-//        Jokes   jokes = new Jokes() ;
-//        String  joke = jokes.getJoke() ;
-//        Intent  intent = new Intent(this, JokeTellActivity.class) ;
-//        intent.putExtra(JOKE_KEY, joke) ;
-//        startActivity(intent);
     }
 
 
